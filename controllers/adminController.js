1c494a77a9580c8fffd4fca80c9c69ff51d268f8
Peter C <peter03311@gmail.com>
2020-07-03 13:44:06 +0800

    Add admin restaurant create page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,28 @@ let adminController = {
     return Restaurant.findAll({ raw: true }).then(restaurants => {
       return res.render('admin/restaurants',{ restaurants: restaurants })
     })
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "Name didn't exits")
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hour: req.body.opening_hour,
+      description: req.body.description
+    })
+    .then((restaurant) => {
+      req.flash('success_messages', "Restaurant was successfully created.")
+      res.redirect('/admin/restaurants')
+    })
   }
 }
 

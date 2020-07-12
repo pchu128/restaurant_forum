ca2818f6cef9f513a71141d456fa6380024bac18
Peter C <peter03311@gmail.com>
2020-07-12 21:59:37 +0800

    feat: Add like/unlike button in restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -30,6 +30,7 @@ let restController = {
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
         isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id),
+        isLiked: req.user.LikedRestaurants.map(d => d.id).includes(r.id),
         categoryName: r.Category.name
       }))
       Category.findAll({
@@ -54,13 +55,16 @@ let restController = {
      include: [
        Category,
        { model: User, as: 'FavoritedUsers' },
+       { model: User, as: 'LikedUsers' },
        { model: Comment, include: [User]}
      ]
    }).then(restaurant => {
      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
+     const isLiked = restaurant.LikedUsers.map(d => d.id).includes(req.user.id)
      return res.render('restaurant', {
        restaurant: restaurant.toJSON(), 
-       isFavorited: isFavorited
+       isFavorited: isFavorited,
+       isLiked: isLiked
      })
    })
   },
